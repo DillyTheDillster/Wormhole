47be0342f7ba234e308b157bcd7c590badbaaafc
the-Astra <djs8743@rit.edu>
2026-02-22 14:45:56 -0500

    Add file blacklist to allow adding to

--- main.lua
+++ main.lua
@@ -1,7 +1,11 @@
 Wormhole = SMODS.current_mod
 
 if PotatoPatchUtils then
-    PotatoPatchUtils.load_files(Wormhole.path .. '/content', Wormhole.id)
+    local file_blacklist = {
+        -- Format entries as `['filename.txt'] = true`
+    }
+
+    PotatoPatchUtils.load_files(Wormhole.path .. '/content', Wormhole.id, file_blacklist)
     SMODS.handle_loc_file(Wormhole.path, Wormhole.id)
     PotatoPatchUtils.LOC.init()
 end
