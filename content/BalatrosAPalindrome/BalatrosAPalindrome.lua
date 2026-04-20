172c12106375d0ab8b64c3a16bc248c0931666c8
BenRoffey <ben.roffey@hotmail.co.uk>
2026-04-20 13:19:26 +0100

    Fixes #16 - Additional draw checks

--- content/BalatrosAPalindrome/BalatrosAPalindrome.lua
+++ content/BalatrosAPalindrome/BalatrosAPalindrome.lua
@@ -59,8 +59,8 @@ SMODS.Edition {
 		return self.weight
 	end,
 	draw = function(self, card, layer)
-		if love.system.getOS() ~= "Linux" then
-			if (layer == 'card' or layer == 'both') and card.sprite_facing == 'front' and (card.config.center.discovered or card.bypass_discovery_center) then
+		if love.system.getOS() ~= "Linux" then --											draw if discovered,				or bypass discovery,			or if showing in a menu (collection) and the card is a playing card (not a joker)
+			if (layer == 'card' or layer == 'both') and card.sprite_facing == 'front' and (card.config.center.discovered or card.bypass_discovery_center or (card.area.config.type == "title" and card.config.center == G.P_CENTERS.c_base)) then
 				card.children.center:draw_shader('worm_bap_shader_void', nil, card.ARGS.send_to_shader)
 				if card.children.front then
 					card.children.front:draw_shader('worm_bap_shader_void', nil, card.ARGS.send_to_shader)
