ad4c0cd672f473980b97d7d7c28fa9939c764787
the-Astra <djs8743@rit.edu>
2026-04-26 16:08:30 -0400

    Fix Disintegration Loop

--- content/violentviolets/eventhorizon.lua
+++ content/violentviolets/eventhorizon.lua
@@ -1,24 +1,22 @@
 SMODS.Joker {
     key = "fraudthird",
     loc_vars = function(self, info_queue, card)
-    return { vars = { card.ability.extra.x_mult } }
+        return { vars = { card.ability.extra.x_mult } }
     end,
     config = { extra = { x_mult = 1.75 } },
     rarity = 3,
     cost = 8,
     atlas = 'VVjokers',
-    pos = {x = 0, y = 0},
+    pos = { x = 0, y = 0 },
     ppu_team = { "Violent Violets" },
     ppu_artist = { "Gud" },
     ppu_coder = { "FireIce" },
-    attributes = {"xmult", "rank", "three", "space"},
+    attributes = { "xmult", "rank", "three", "space" },
     calculate = function(self, card, context)
-        if context.individual and context.cardarea == G.play then
-            if context.other_card:get_id() == 3 and context.other_card:get_id() == 8 then
-                return {
+        if context.individual and context.cardarea == G.play and (context.other_card:get_id() == 3 or context.other_card:get_id() == 8) then
+            return {
                 x_mult = card.ability.extra.x_mult
-                }
-            end
+            }
         end
     end
 }
