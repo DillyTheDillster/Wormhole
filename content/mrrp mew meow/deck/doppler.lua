37bb2041b9d0692ed4acf5ee61166bcf3e33e081
the-Astra <djs8743@rit.edu>
2026-04-26 13:04:31 -0400

    Fix #87

--- content/mrrp mew meow/deck/doppler.lua
+++ content/mrrp mew meow/deck/doppler.lua
@@ -14,7 +14,7 @@ SMODS.Back {
         }
     end,
     calculate = function(self, back, context)
-        if context.using_consumeable and context.consumeable.ability.set == 'Planet' then
+        if context.using_consumeable and context.consumeable.ability.set == 'Planet' and context.consumeable.ability.consumeable.hand_type then
             local card = context.consumeable
             local from = G.deck and G.deck.cards[1] or G.deck or card
             local from_hand = card.ability.consumeable.hand_type
--- content/vegas/wormhole.lua
+++ content/vegas/wormhole.lua
@@ -1384,20 +1384,6 @@ SMODS.Consumable {
     end
 }
 
-SMODS.PokerHand{
-	key = "keplerhand",
-	chips = 4,
-	mult = 1, 
-	l_chips = 0,
-	l_mult = 0,
-	example = {},
-	visible = false,
-	evaluate = function(parts, hand)
-		return false
-	end,
-	loc_txt = {},
-	no_collection = true
-}
 SMODS.Consumable {
     key = 'kepler',
     set = 'Planet',
@@ -1407,7 +1393,7 @@ SMODS.Consumable {
  	end,
     pos = { x = 0, y = 0 },
 	discovered = true,
-    config = { xmult = 0.1, max_highlighted = 2, hand_type = "worm_keplerhand"},
+    config = { xmult = 0.1, max_highlighted = 2 },
 	ppu_team = {"People Found In Vegas"},
     ppu_coder = {"Ben Roffey"},
     ppu_artist = {"Ben Roffey"},
