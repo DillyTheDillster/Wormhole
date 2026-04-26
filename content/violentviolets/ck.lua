ad4e662f4e2ed98764683875dac800b608974710
the-Astra <djs8743@rit.edu>
2026-04-26 16:05:33 -0400

    Fix Crescent King + Fix sprite bleeding

--- content/violentviolets/ck.lua
+++ content/violentviolets/ck.lua
@@ -3,8 +3,8 @@ SMODS.Joker {
     atlas = 'VVjokers',
     rarity = 2,
     cost = 4,
-    pos = {x = 2, y = 1},
-    soul_pos = {x = 3, y = 1},
+    pos = { x = 2, y = 1 },
+    soul_pos = { x = 3, y = 1 },
     config = {
         extra = {
         },
@@ -17,16 +17,16 @@ SMODS.Joker {
     ppu_team = { "Violent Violets" },
     ppu_artist = { "FirstTry" },
     ppu_coder = { "FirstTry" },
-    attributes = {"hand_type", "space"},
-    loc_vars = function(self,info_queue,card)
+    attributes = { "hand_type", "space" },
+    loc_vars = function(self, info_queue, card)
         local _handname, _played = 'High Card', -1
-            for hand_key, hand in pairs(G.GAME.hands) do
-                if hand.played > _played then
-                    _played = hand.played
-                    _handname = hand_key
-                end
+        for hand_key, hand in pairs(G.GAME.hands) do
+            if hand.played > _played then
+                _played = hand.played
+                _handname = hand_key
             end
-            local most_played = _handname
+        end
+        local most_played = _handname
         return {
             vars = {
                 card.ability.immutable.rounds,
@@ -35,34 +35,28 @@ SMODS.Joker {
             }
         }
     end,
-    calculate = function(self,card,context)
-    local round_tally = 0
-    local _handname, _played = 'High Card', -1
-            for hand_key, hand in pairs(G.GAME.hands) do
-                if hand.played > _played then
-                    _played = hand.played
-                    _handname = hand_key
-                end
+    calculate = function(self, card, context)
+        local round_tally = 0
+        local _handname, _played = 'High Card', -1
+        for hand_key, hand in pairs(G.GAME.hands) do
+            if hand.played > _played then
+                _played = hand.played
+                _handname = hand_key
             end
-            local most_played = _handname
-            if context.end_of_round and context.main_eval then
---            SMODS.calculate_effect({ message = card.ability.immutable.roll_rounds .."/".. card.ability.immutable.total_rounds , colour = G.C.FILTER}, card)
-            SMODS.scale_card(card, {
-                ref_table = card.ability.immutable,
-                ref_value = "roundup",
-                scalar_value = "roundadd",
---                message = card.ability.immutable.roll_rounds .."/".. card.ability.immutable.total_rounds,
---                colour = G.C.RED
-            })
-        if (card.ability.immutable.roundup >= card.ability.immutable.rounds) then
-            SMODS.upgrade_poker_hands{
-                        from = card,
-                        parameters = { "chips", "mult"},
-                        level_up = 1,
-                        hands = most_played
-                    }
-                    card.ability.immutable.roundup = 0
-                end
+        end
+        local most_played = _handname
+        if context.end_of_round and context.main_eval then
+            card.ability.immutable.roundup = card.ability.immutable.roundup + card.ability.immutable.roundadd
+            SMODS.calculate_effect({ message = card.ability.immutable.roundup .. "/" .. card.ability.immutable.rounds, colour = G.C.RED}, card)
+            if (card.ability.immutable.roundup >= card.ability.immutable.rounds) then
+                SMODS.upgrade_poker_hands {
+                    from = card,
+                    parameters = { "chips", "mult" },
+                    level_up = 1,
+                    hands = most_played
+                }
+                card.ability.immutable.roundup = 0
             end
+        end
     end
 }
