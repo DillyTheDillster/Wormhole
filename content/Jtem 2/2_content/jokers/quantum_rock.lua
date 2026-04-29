8b415c406f99183612618920ad8cbe00c8ec1ec7
WilsontheWolf <git@shorty.systems>
2026-04-28 19:46:09 -0600

    rock shop trigger and rare

--- content/Jtem 2/2_content/jokers/quantum_rock.lua
+++ content/Jtem 2/2_content/jokers/quantum_rock.lua
@@ -9,18 +9,6 @@ SMODS.Gradient({
 	cycle = 1,
 })
 
-SMODS.Rarity({
-	key = "jtem2_quantum",
-	default_weight = 0.03,
-	badge_colour = SMODS.Gradients["worm_jtem2_quantum"],
-	pools = {
-	    Joker = true,
-	},
-	get_weight = function(self, weight, object_type)
-		return weight
-	end,
-})
-
 SMODS.Atlas({
 	key = "jtem2_quantum_rock",
 	path = "Jtem 2/jokers/quantum_rock.png",
@@ -119,126 +107,148 @@ local get_info_for_info_queue = function(name_string) --quick fix
 	if name_string == "Frozen Card"		then return G.P_CENTERS.m_worm_ibu_frozen end
 end
 
+local function checkShop(self, area)
+    if G.GAME.worm_quantum_rock_spawned or area.config.type == "shop" then
+	print("shopped")
+	self.set_card_area = nil
+	G.GAME.worm_quantum_rock_spawned = true
+    end
+    return Card.set_card_area(self, area)
+end
+
 local rock = SMODS.Joker({
-	key = "jtem2_quantum_rock",
-	discovered = true,
-	unlocked = true,
-	ppu_team = { "jtem2" },
-	ppu_coder = { "sleepyg11" },
-	ppu_artist = { "aikoyori" },
+    key = "jtem2_quantum_rock",
+    discovered = true,
+    unlocked = true,
+    ppu_team = { "jtem2" },
+    ppu_coder = { "sleepyg11" },
+    ppu_artist = { "aikoyori" },
 
-	replace_base_card = true,
+    replace_base_card = true,
 
-	atlas = "worm_jtem2_quantum_rock",
-	pos = { x = 0, y = 0 },
+    atlas = "worm_jtem2_quantum_rock",
+    pos = { x = 0, y = 0 },
 
-	rarity = "worm_jtem2_quantum",
-	cost = 3,
+    rarity = 3,
+    cost = 3,
 
-	config = {
-		Xmult = 3,
-	},
+    config = {
+	Xmult = 3,
+    },
 
-	attributes = {"xmult", "space"},
+    attributes = {"xmult", "space"},
 
-	
-	loc_vars = function(self, info_queue, card)
-		if card.ability.effect then
-			info_queue[#info_queue + 1] = get_info_for_info_queue(card.ability.effect)
-		end
-		return {
-			set = "Joker",
-			type = "descriptions",
-			key = "j_worm_jtem2_quantum_rock",
-			vars = {
-				card.ability.Xmult,
-				localize("Jack", "ranks"),
-				localize("Spades", "suits_plural"),
-				colours = { G.C.SUITS["Spades"] },
-			},
-		}
-	end,
 
-	generate_ui = function(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
-					
-		perma_vars = specific_vars
-		specific_vars = {}					--when enhanced, specific_vars contains playing_card = true, suit = spades, rank = jack etc
-											--removing so it reverts to the joker definition hopefully. (but this isn't the solution)
+    loc_vars = function(self, info_queue, card)
+	if card.ability.effect then
+	    info_queue[#info_queue + 1] = get_info_for_info_queue(card.ability.effect)
+	end
+	return {
+	    set = "Joker",
+	    type = "descriptions",
+	    key = "j_worm_jtem2_quantum_rock",
+	    vars = {
+		card.ability.Xmult,
+		localize("Jack", "ranks"),
+		localize("Spades", "suits_plural"),
+		colours = { G.C.SUITS["Spades"] },
+	    },
+	}
+    end,
 
-		full_UI_table.card_type = "Joker"	--when enhanced, card_type changes to "Enhanced". Forcing it to be "Joker" so the UI doesn't break
-		full_UI_table.name = nil			--when enchanced, name = true. Forcing it to be nil so "Quantum Rock" gets displayed
+    generate_ui = function(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
 
-		
-		if not card then					--see smods/src/game_object.lua - line 1232 - SMODS.Centers.generate_ui
-            card = self:create_fake_card()
-        end
-        local target = {
-            type = 'descriptions',
-            key = "j_worm_jtem2_quantum_rock",
-            set = "Joker",
-            nodes = desc_nodes,
-            AUT = full_UI_table,
-            vars = specific_vars or {}
-        }
-        local res = {}
-        if self.loc_vars and type(self.loc_vars) == 'function' then
-            res = self:loc_vars(info_queue, card) or {}
-            target.vars = res.vars or target.vars
-            target.key = res.key or target.key
-            target.set = res.set or target.set
-            target.scale = res.scale
-            target.text_colour = res.text_colour
-            if desc_nodes == full_UI_table.main then
-                full_UI_table.box_starts = res.box_starts
-                full_UI_table.box_ends = res.box_ends
-            end
-        end
+	perma_vars = specific_vars
+	specific_vars = {}					--when enhanced, specific_vars contains playing_card = true, suit = spades, rank = jack etc
+	--removing so it reverts to the joker definition hopefully. (but this isn't the solution)
 
-        if desc_nodes == full_UI_table.main and not full_UI_table.name then
-            full_UI_table.name = localize { type = 'name', set = res.name_set or target.set, key = res.name_key or target.key, nodes = full_UI_table.name, vars = res.name_vars or target.vars or {} } --removed self.set == 'Enhanced' and 'temp_value' or ...
-        elseif desc_nodes ~= full_UI_table.main and not desc_nodes.name then
-            desc_nodes.name = localize{type = 'name_text', key = res.name_key or target.key, set = res.name_set or target.set }
-            if (not full_UI_table.from_detailed_tooltip or full_UI_table.info[1] == desc_nodes) 
-                and not full_UI_table.no_styled_name then
-                desc_nodes.name_styled = {}
-    
-                localize{type = 'name', key = res.name_key or target.key, set = res.name_set or target.set, nodes = desc_nodes.name_styled, fixed_scale = 0.63, no_pop_in = true, no_shadow = true, y_offset = 0, no_spacing = true, no_bump = true, vars = res.name_vars or target.vars} 
-                desc_nodes.name_styled = SMODS.info_queue_desc_from_rows(desc_nodes.name_styled, true)
-                desc_nodes.name_styled.config.align = "cm"
-            end
-        end
-        if specific_vars and specific_vars.debuffed and not res.replace_debuff then
-            target = { type = 'other', key = 'debuffed_' ..
-            (specific_vars.playing_card and 'playing_card' or 'default'), nodes = desc_nodes, AUT = full_UI_table, }
-        end
-        if res.main_start then
-            desc_nodes[#desc_nodes + 1] = res.main_start
-        end
+	full_UI_table.card_type = "Joker"	--when enhanced, card_type changes to "Enhanced". Forcing it to be "Joker" so the UI doesn't break
+	full_UI_table.name = nil			--when enchanced, name = true. Forcing it to be nil so "Quantum Rock" gets displayed
 
-		if card.ability.perma_bonus and card.ability.perma_bonus ~= 0 then
-			localize{type = 'other', key = 'card_extra_chips', nodes = desc_nodes, vars = {card.ability.perma_bonus}}
-		end
-		SMODS.localize_perma_bonuses(convert_perma_to_bonus_vars(card.ability), desc_nodes) --see SMODS/src/utils.lua - line 3282
 
-        localize(target)
-        if res.main_end then
-            desc_nodes[#desc_nodes + 1] = res.main_end
-        end
-        desc_nodes.background_colour = res.background_colour
+	if not card then					--see smods/src/game_object.lua - line 1232 - SMODS.Centers.generate_ui
+	    card = self:create_fake_card()
+	end
+	local target = {
+	    type = 'descriptions',
+	    key = "j_worm_jtem2_quantum_rock",
+	    set = "Joker",
+	    nodes = desc_nodes,
+	    AUT = full_UI_table,
+	    vars = specific_vars or {}
+	}
+	local res = {}
+	if self.loc_vars and type(self.loc_vars) == 'function' then
+	    res = self:loc_vars(info_queue, card) or {}
+	    target.vars = res.vars or target.vars
+	    target.key = res.key or target.key
+	    target.set = res.set or target.set
+	    target.scale = res.scale
+	    target.text_colour = res.text_colour
+	    if desc_nodes == full_UI_table.main then
+		full_UI_table.box_starts = res.box_starts
+		full_UI_table.box_ends = res.box_ends
+	    end
+	end
 
-	end,
+	if desc_nodes == full_UI_table.main and not full_UI_table.name then
+	    full_UI_table.name = localize { type = 'name', set = res.name_set or target.set, key = res.name_key or target.key, nodes = full_UI_table.name, vars = res.name_vars or target.vars or {} } --removed self.set == 'Enhanced' and 'temp_value' or ...
+	elseif desc_nodes ~= full_UI_table.main and not desc_nodes.name then
+	    desc_nodes.name = localize{type = 'name_text', key = res.name_key or target.key, set = res.name_set or target.set }
+	    if (not full_UI_table.from_detailed_tooltip or full_UI_table.info[1] == desc_nodes) 
+		and not full_UI_table.no_styled_name then
+		desc_nodes.name_styled = {}
 
-	calculate = function(self, card, context)
-		if context.joker_main or (context.main_scoring and context.cardarea == G.play) then
-			return {
-				xmult = card.ability.Xmult,
-			}
-		end
-	end,
+		localize{type = 'name', key = res.name_key or target.key, set = res.name_set or target.set, nodes = desc_nodes.name_styled, fixed_scale = 0.63, no_pop_in = true, no_shadow = true, y_offset = 0, no_spacing = true, no_bump = true, vars = res.name_vars or target.vars} 
+		desc_nodes.name_styled = SMODS.info_queue_desc_from_rows(desc_nodes.name_styled, true)
+		desc_nodes.name_styled.config.align = "cm"
+	    end
+	end
+	if specific_vars and specific_vars.debuffed and not res.replace_debuff then
+	    target = { type = 'other', key = 'debuffed_' ..
+	    (specific_vars.playing_card and 'playing_card' or 'default'), nodes = desc_nodes, AUT = full_UI_table, }
+	end
+	if res.main_start then
+	    desc_nodes[#desc_nodes + 1] = res.main_start
+	end
 
-	add_to_deck = function()
-	    G.GAME.worm_quantum_rock_spawned = true
-	end,
+	if card.ability.perma_bonus and card.ability.perma_bonus ~= 0 then
+	    localize{type = 'other', key = 'card_extra_chips', nodes = desc_nodes, vars = {card.ability.perma_bonus}}
+	end
+	SMODS.localize_perma_bonuses(convert_perma_to_bonus_vars(card.ability), desc_nodes) --see SMODS/src/utils.lua - line 3282
+
+	localize(target)
+	if res.main_end then
+	    desc_nodes[#desc_nodes + 1] = res.main_end
+	end
+	desc_nodes.background_colour = res.background_colour
+
+    end,
+
+    calculate = function(self, card, context)
+	if context.joker_main or (context.main_scoring and context.cardarea == G.play) then
+	    return {
+		xmult = card.ability.Xmult,
+	    }
+	end
+    end,
+
+    add_to_deck = function()
+	G.GAME.worm_quantum_rock_spawned = true
+    end,
+    set_card_type_badge = function(self, card, badges)
+	badges[#badges + 1] = create_badge(
+	    localize('k_worm_jtem2_quantum'),
+	    SMODS.Gradients.worm_jtem2_quantum,
+	    G.C.WHITE,
+	    1.2
+	)
+    end,
+    set_ability = function(self, card, initial, delay_sprites)
+	if not G.GAME.worm_quantum_rock_spawned then
+	    card.set_card_area = checkShop
+	end
+    end,
 })
 
 local function is_rock_present()
