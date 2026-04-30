51087c6f9a9118189a224524b20d9634a9e705c0
the-Astra <djs8743@rit.edu>
2026-04-30 11:53:40 -0400

    Setting this to true made it not appear on the actual credits screen

--- content/colon_three/zzz_main.lua
+++ content/colon_three/zzz_main.lua
@@ -113,7 +113,7 @@ SMODS.DrawStep {
         self.children.center:draw_shader("polychrome", nil, self.ARGS.send_to_shader)
         self.children.center:set_sprite_pos({ x = 3, y = 0 })
     end,
-    conditions = { vortex = true, facing = 'front' },
+    conditions = { vortex = false, facing = 'front' },
 }
 
 -- rotate hook
